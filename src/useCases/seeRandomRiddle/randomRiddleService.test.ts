05cb72ad598a2d6a9cdd7557e2de0a83ad4da332
Nik Sumeiko <nik.sumeiko@gmail.com>
2025-06-09 15:56:21 +0200

    test: Timestamp assertion

--- src/useCases/seeRandomRiddle/randomRiddleService.test.ts
+++ src/useCases/seeRandomRiddle/randomRiddleService.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { createARandomRiddleModel } from './randomRiddleService';
 
 describe('randomRiddleService', () => {
@@ -17,10 +17,13 @@ describe('randomRiddleService', () => {
     });
 
     it('returns model with sleepies work interval', () => {
-        const date = new Date('2025-06-09T23:30:00');
-        const riddle = createARandomRiddleModel(date);
+        const dates = [new Date('2025-06-09T23:30:00'), new Date('2025-06-09T04:00:00')];
 
-        expect(riddle.workInterval).toBe('Sleepies');
+        dates.forEach((date) => {
+            const riddle = createARandomRiddleModel(date);
+
+            expect(riddle.workInterval).toBe('Sleepies');
+        });
     });
 
     it('returns model with busy times work interval', () => {
@@ -28,5 +31,13 @@ describe('randomRiddleService', () => {
         const riddle = createARandomRiddleModel(date);
 
         expect(riddle.workInterval).toBe('Busy Times');
+    });
+
+    it('returns model with timestamp', () => {
+        const date = new Date('2025-06-09T23:30:00');
+
+        const result = createARandomRiddleModel(date);
+
+        expect(result.timestamp).toBe('2025-06-09 23:30');
     });
 });
