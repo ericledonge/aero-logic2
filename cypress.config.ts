fc282a2573c4c5d7a522253569f83e02f7d3a064
Nik Sumeiko <nik.sumeiko@gmail.com>
2025-06-12 17:02:33 +0200

    chore: Disable requests from API and external hosts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  env: {
-    uncaughtCypressException: false,
-    hideXhr: true,
-  },
-  chromeWebSecurity: false,
-  // blockHosts: ['!*localhost*'],
-  e2e: {
-    baseUrl: 'http://localhost:3000',
-    specPattern: 'src/**/__tests__/*.integration.ts',
-    viewportWidth: 680,
-    viewportHeight: 768,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    env: {
+        uncaughtCypressException: false,
+        hideXhr: true,
     },
-  },
+    chromeWebSecurity: false,
+    blockHosts: ['!*localhost*', '*api/*'],
+    e2e: {
+        baseUrl: 'http://localhost:3000',
+        specPattern: 'src/**/__tests__/*.integration.ts',
+        viewportWidth: 680,
+        viewportHeight: 768,
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+    },
 });
